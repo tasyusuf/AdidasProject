6ac65cc446e7712ef48c85cfc78fba1effdb37b4
byTass <ytas9293@gmail.com>
2022-02-06 18:14:40 +0300

    adidas begins

---
+++ src/test/java/com/demoblaze/step_definitions/PurchaseSteps.java
@@ -0,0 +1,16 @@
+package com.demoblaze.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class PurchaseSteps {
+    @Given("User is on the home page")
+    public void user_is_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User adds {string} from {string}")
+    public void user_adds_from(String product, String category) {
+
+    }
+}
