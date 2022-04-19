9ad2f446e428dd031080315e8b0d668bb853b767
byTass <ytas9293@gmail.com>
2022-04-19 16:49:14 +0300

    adidas begins

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                "html:target/cucumber-report.html"},
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
         dryRun = false,
